3da1864c8ab883b79e2011faea1859df89071951
Astro-Syn <kelseyamyb@gmail.com>
2025-08-20 00:15:12 -0400

    work on message window

--- src/pages/msn/msn_chat_window/MsnChatWindow.tsx
+++ src/pages/msn/msn_chat_window/MsnChatWindow.tsx
@@ -61,18 +61,118 @@ style={{
         <div className='msn-chat-window'>
 
             {/*Standard Blue Title */}
-            <div className='msn-chat-window-header'>
-                Header
+            <div className='standard-title-bar'
+            onMouseDown={onMouseDown}
+            >
+                <div className='header-logo-container'>
+                    <img
+                    src='/Images/msn_chat_invite.png'
+                    />
+                    <span className='msn-chat-window-title'>    
+                        Novie Chat
+                    </span>
+                    
+                </div>
+                <div className='header-line'>
+
+                    </div>
+
+                <div className='msn-chat-window-buttons'>
+                    <button onClick={() => setIsMinimized(!isMinimized)}>-</button>
+                    <button onClick={() => setIsMaximized(!isMaximized)}>🗖</button>
+                    <button onClick={() => setIsVisible(false)}>×</button>
+                </div>
             </div>
+
+            {!isMinimized && ( 
+                <div className='msn-chat-window-content'>
+
+                </div>
+            )}
             <div className='msn-chat-window-header-two'>
             File, edit, actions, tools
             </div>
 
             {/*Curvy Header */}
             <div className='msn-chat-window-header-three-container'>
-                <div className='msn-chat-window-header-three-left'>
-                    Invite, Send Files, Video, Voice
-                </div>
+                
+
+  <div className="banner">
+  <svg viewBox="0 500 2200 1000" xmlns="http://www.w3.org/2000/svg">
+  
+    <defs>
+  <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+    <stop offset="10%" stopColor="#DCEFF5" />
+    <stop offset="50%" stopColor="#A3C5CC" />
+    <stop offset="100%" stopColor="#DCEFF5" />
+  </linearGradient>
+</defs>
+
+    <path
+     d='M 4040 640 Q 1440 640 1200 640 C 1120 640 1120 800 1040 800 Q 880 800 -400 800 C -400 720 -400 560 -400 480 V 480 L 2940 480'
+      fill="url(#waveGradient)"
+      stroke="gray"
+      strokeWidth="5"
+    />
+  </svg>
+
+  <div className='inner-content'>
+    <button
+    className='up-down-btn'
+    >
+        <img
+        
+        src='/Images/down_up_btn.png'
+        />
+    </button>
+    <div className='options-buttons-container'>
+            <button>
+        <img
+        src='/Images/msn_chat_invite.png'
+        />
+        <p>Invite</p>
+    </button>
+    <button>
+        <img
+        src='/Images/msn_chat_send_files.png'
+        />
+        <p>Send Files</p>
+    </button>
+    <button>
+        <img
+        src='/Images/msn_chat_voice.png'
+        />
+        <p>Voice</p>
+    </button>
+    <button>
+        <img
+        src='/Images/msn_chat_activities.png'
+        />
+        <p>Activities</p>
+    </button>
+    <button>
+        <img
+        src='/Images/msn_chat_games.png'
+        />
+        <p>Games</p>
+    </button>
+    
+    
+    </div>
+    
+    <div className='msn-chat-window-logo'>
+            <img
+            src='/Images/msn_logo.png'
+            />
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
                 <div className='msn-chat-window-header-three-right'>
                     Block, customize
                 </div>
@@ -86,10 +186,11 @@ style={{
                     <MsnChat/>
                 </div>
                 <div className='msn-chat-display-picture-box'>
-                    <div className='msn-chat-display-pictire'>
+                    <div className='msn-chat-display-picture'>
                         <img/>
                     </div>
                 </div>
+                
 
                 </div>
                 
